d714e57946abd050c620bb08a7d3ce0e022704b8
Park-SM <park97.sm@gmail.com>
2019-02-17 19:06:51 +0900

    CreateEnemy() commit

--- DefenseGame/Enemy.c
+++ DefenseGame/Enemy.c
@@ -1,1 +1,13 @@
 #include "Enemy.h"
+
+ENEMY* CreateEnemy(int gate) {
+	ENEMY* NewEnemy = (ENEMY*)malloc(sizeof(ENEMY));
+
+	NewEnemy->type = 'e';
+	NewEnemy->x = gate;
+	NewEnemy->y = 2;
+	NewEnemy->shape = "¥ø";
+	NewEnemy->NextEnemy = NULL;
+
+	return NewEnemy;
+}
