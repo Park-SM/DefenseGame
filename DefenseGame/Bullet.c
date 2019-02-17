8bb26d1c41ecdb74c96b58c7e492450f51381d3f
Leemh <mzerin@naver.com>
2019-02-17 19:07:50 +0900

    CreateBullet Commit

--- DefenseGame/Bullet.c
+++ DefenseGame/Bullet.c
@@ -1,1 +1,13 @@
 #include "Bullet.h"
+
+BULLET* CreateEnemy(int x)
+{
+	BULLET* NewBullet = (BULLET *)malloc(sizeof(BULLET));
+	
+	NewBullet->type = 'b';
+	NewBullet->x = x;
+	NewBullet->y = 22;
+	NewBullet->shape = "ก่";
+
+	return NewBullet;
+}
